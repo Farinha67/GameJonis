8d0315302778aab028e695865aad1334d4086df7
Andriel <167346031+Farinha67@users.noreply.github.com>
2026-08-17 16:06:26 -0300

    Palyer

---
+++ GameJan26/Assets/Andriel/PlayerPickup.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerPickup : MonoBehaviour
+{
+    [Header("Holding Point")]
+    public Transform holdingPoint;
+
+    [Header("Pickup")]
+    public float pickupDistance = 3f;
+
+    private GameObject heldObject;
+
+    void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            if (heldObject == null)
+            {
+                TryPickup();
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+    }
+
+    void TryPickup()
+    {
+        Ray ray = new Ray(
+            Camera.main.transform.position,
+            Camera.main.transform.forward
+        );
+
+        if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance))
+        {
+            if (hit.collider.CompareTag("Pickup"))
+            {
+                heldObject = hit.collider.gameObject;
+
+                Rigidbody rb = heldObject.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    rb.isKinematic = true;
+                    rb.useGravity = false;
+                }
+
+                Collider col = heldObject.GetComponent<Collider>();
+
+                if (col != null)
+                {
+                    col.enabled = false;
+                }
+
+                heldObject.transform.SetParent(holdingPoint);
+
+                heldObject.transform.localPosition = Vector3.zero;
+                heldObject.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        heldObject.transform.SetParent(null);
+
+        heldObject.transform.position =
+            Camera.main.transform.position +
+            Camera.main.transform.forward * 1.5f;
+
+        Rigidbody rb = heldObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
+
+        Collider col = heldObject.GetComponent<Collider>();
+
+        if (col != null)
+        {
+            col.enabled = true;
+        }
+
+        heldObject = null;
+    }
+}
