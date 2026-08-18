f4c557167427bf135ba907710dfac5937da9a084
luiz62730 <luiz60581556@alunos.sc.senac.br>
2026-08-18 09:03:54 -0300

    feito alguns ajuste no scripts

--- GameJan26/Assets/Luiz/Scripts/CameraEntrandoNoCeleiro.cs
+++ GameJan26/Assets/Luiz/Scripts/CameraEntrandoNoCeleiro.cs
@@ -4,17 +4,17 @@ public class CameraEntrandoNoCeleiro : MonoBehaviour
 {
     [Header("Configurações da Câmera")]
     public Transform mainCamera;
-    public Transform insideBarnPosition; // Aquele Ponto B escuro lá dentro da casinha
-    public float transitionSpeed = 1.5f; // Diminua esse número se quiser que a câmera ande mais devagar!
+    public Transform insideBarnPosition; 
+    public float transitionSpeed = 1.5f; 
 
     [Header("Telas de Interface (UI)")]
-    public GameObject mainMenuCanvas;    // O seu Canvas com os botões (Jogar, Opções, Sair)
+    public GameObject mainMenuCanvas;    
 
     private bool isMoving = false;
 
     void Start()
     {
-        // Assim que a cena carregar, esconde os botões e avisa a câmera para começar a andar
+        
         if (mainMenuCanvas != null) mainMenuCanvas.SetActive(false);
         isMoving = true;
     }
@@ -23,15 +23,15 @@ public class CameraEntrandoNoCeleiro : MonoBehaviour
     {
         if (isMoving)
         {
-            // O comando Lerp faz a câmera DESLIZAR suavemente até lá dentro, sem teleportar
+           
             mainCamera.position = Vector3.Lerp(mainCamera.position, insideBarnPosition.position, transitionSpeed * Time.deltaTime);
             mainCamera.rotation = Quaternion.Lerp(mainCamera.rotation, insideBarnPosition.rotation, transitionSpeed * Time.deltaTime);
 
-            // Quando a câmera chegar bem pertinho do Ponto B, ela para
+            
             if (Vector3.Distance(mainCamera.position, insideBarnPosition.position) < 0.1f)
             {
-                isMoving = false; // Para a câmera
-                if (mainMenuCanvas != null) mainMenuCanvas.SetActive(true); // Mostra os seus botões
+                isMoving = false; 
+                if (mainMenuCanvas != null) mainMenuCanvas.SetActive(true); 
             }
         }
     }
