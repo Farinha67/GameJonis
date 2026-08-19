3636ff2262e2068c046b1f334825347a985c03d3
Jullya2 <jullya.monteiro@alunos.sc.senac.br>
2026-08-18 23:33:14 -0300

    coloquei a dublagem

--- GameJan26/Assets/Luiz/Scripts/CenaNarrativa.cs
+++ GameJan26/Assets/Luiz/Scripts/CenaNarrativa.cs
@@ -1,57 +1,66 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CenaNarrativa : MonoBehaviour
 {
-    [Header("Áudio da narrativa")]
     public AudioSource audioNarracao;
-
-    [Header("Jogador")]
     public MonoBehaviour controleDoJogador;
-
-    [Header("Objetos da narrativa")]
     public GameObject painelNarrativa;
+    public GameObject botaoPular;
+    public string cenaDoJogo;
 
     private bool narrativaTerminou = false;
 
     void Start()
     {
-
         if (controleDoJogador != null)
             controleDoJogador.enabled = false;
-
 
         if (painelNarrativa != null)
             painelNarrativa.SetActive(true);
 
+        if (botaoPular != null)
+            botaoPular.SetActive(false);
 
-        if (audioNarracao != null)
-            audioNarracao.Play();
+        Invoke("IniciarNarracao", 5f);
     }
 
     void Update()
     {
-
         if (!narrativaTerminou &&
             audioNarracao != null &&
+            audioNarracao.clip != null &&
             !audioNarracao.isPlaying &&
-            audioNarracao.time > 0)
+            audioNarracao.time > 0f)
         {
             IniciarJogo();
         }
     }
 
+    void IniciarNarracao()
+    {
+        if (audioNarracao != null)
+            audioNarracao.Play();
+
+        if (botaoPular != null)
+            botaoPular.SetActive(true);
+    }
+
+    public void PularNarrativa()
+    {
+        IniciarJogo();
+    }
+
     void IniciarJogo()
     {
+        if (narrativaTerminou)
+            return;
+
         narrativaTerminou = true;
 
+        if (audioNarracao != null)
+            audioNarracao.Stop();
 
-        if (painelNarrativa != null)
-            painelNarrativa.SetActive(false);
-
-
-        if (controleDoJogador != null)
-            controleDoJogador.enabled = true;
-
-        Debug.Log("Narrativa terminou. Jogo iniciado!");
+        SceneManager.LoadScene(cenaDoJogo);
     }
 }
